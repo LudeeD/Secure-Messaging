77bb6f1a4fd5d75b30558c21a9362111d1fb6ca9
Luís Silva <lfssilva@ua.pt>
2017-12-19 19:29:55 +0000

    change the launching server script to handle errors

--- server/runserver.sh
+++ server/runserver.sh
@@ -2,4 +2,9 @@
 
 javac -d bin -sourcepath src -cp lib/gson-2.6.2.jar src/Server.java
 
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mErrors, pls Reverify Code\e[0m"
+    exit 1
+fi
+
 java -cp ./bin:./lib/gson-2.6.2.jar Server 43
